b0af2f044928643955e984038142de5b30576bdb
slows9911 <32136949+slows99@users.noreply.github.com>
2020-11-07 23:21:49 -0500

    Task: Travis setup

---
+++ CsvReader/CsvReader.py
@@ -0,0 +1,27 @@
+import csv
+from pathlib import Path
+
+
+def ClassFactory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
+
+
+
+class CvsReader:
+    data = []
+
+    def __init__(self, filepath):
+        self.data = []
+        relative = Path(filepath)
+        absolute = relative.absolute()
+        with open(absolute) as text_data:
+            csv_data = csv.DictReader(text_data, delimeter= ',')
+            for row in csv_data:
+                self.data.append(row)
+        pass
+
+    def return_data_as_objects(self, class_name):
+        objects = []
+        for row in self.data:
+            objects.append(ClassFactory(class_name, row))
+        return objects
---
+++ Statistics/Statistics.py
@@ -0,0 +1,10 @@
+from calculator.calculator import calculator
+
+
+class Statistics(calculator):
+    data = []
+
+    def = []
+
+    def __init__(self):
+        super().__init__()

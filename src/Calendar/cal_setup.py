e11e5595fd1e31cbb6d2791dab621f42d4a3ca6f
slows9911 <32136949+slows99@users.noreply.github.com>
2020-12-17 17:55:01 -0500

    Creating and Setting up the Email features

--- src/Calendar/cal_setup.py
+++ src/Calendar/cal_setup.py
@@ -1,4 +1,3 @@
-import datetime
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -8,28 +7,27 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-CREDENTIALS_FILE = 'path_to_file/credentials.json'
+CREDENTIALS_FILE = 'credentials.json'
 
 def get_calendar_service():
-   creds = None
-   # The file token.pickle stores the user's access and refresh tokens, and is
-   # created automatically when the authorization flow completes for the first
-   # time.
-   if os.path.exists('token.pickle'):
-       with open('token.pickle', 'rb') as token:
-           creds = pickle.load(token)
-   # If there are no (valid) credentials available, let the user log in.
-   if not creds or not creds.valid:
-       if creds and creds.expired and creds.refresh_token:
-           creds.refresh(Request())
-       else:
-           flow = InstalledAppFlow.from_client_secrets_file(
-               CREDENTIALS_FILE, SCOPES)
-           creds = flow.run_local_server(port=0)
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CREDENTIALS_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-       # Save the credentials for the next run
-       with open('token.pickle', 'wb') as token:
-           pickle.dump(creds, token)
-
-   service = build('calendar', 'v3', credentials=creds)
-   return service
+    service = build('calendar', 'v3', credentials=creds)
+    return service
--- src/Calendar/create_event.py
+++ src/Calendar/create_event.py
@@ -1,30 +1,35 @@
-from datetime import datetime, timedelta
-from cal_setup import get_calendar_service
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-def main():
-   # creates one hour event tomorrow 10 AM IST
-   service = get_calendar_service()
+CREDENTIALS_FILE = 'path_to_file/credentials.json'
 
-   d = datetime.now().date()
-   tomorrow = datetime(d.year, d.month, d.day, 10)+timedelta(days=1)
-   start = tomorrow.isoformat()
-   end = (tomorrow + timedelta(hours=1)).isoformat()
+def get_calendar_service():
+   creds = None
+   # The file token.pickle stores the user's access and refresh tokens, and is
+   # created automatically when the authorization flow completes for the first
+   # time.
+   if os.path.exists('token.pickle'):
+       with open('token.pickle', 'rb') as token:
+           creds = pickle.load(token)
+   # If there are no (valid) credentials available, let the user log in.
+   if not creds or not creds.valid:
+       if creds and creds.expired and creds.refresh_token:
+           creds.refresh(Request())
+       else:
+           flow = InstalledAppFlow.from_client_secrets_file(
+               CREDENTIALS_FILE, SCOPES)
+           creds = flow.run_local_server(port=0)
 
-   event_result = service.events().insert(calendarId='primary',
-       body={
-           "summary": 'Automating calendar',
-           "description": 'This is a tutorial example of automating google calendar with python',
-           "start": {"dateTime": start, "timeZone": 'Asia/Kolkata'},
-           "end": {"dateTime": end, "timeZone": 'Asia/Kolkata'},
-       }
-   ).execute()
+       # Save the credentials for the next run
+       with open('token.pickle', 'wb') as token:
+           pickle.dump(creds, token)
 
-   print("created event")
-   print("id: ", event_result['id'])
-   print("summary: ", event_result['summary'])
-   print("starts at: ", event_result['start']['dateTime'])
-   print("ends at: ", event_result['end']['dateTime'])
-
-if __name__ == '__main__':
-   main()
+   service = build('calendar', 'v3', credentials=creds)
+   return service
